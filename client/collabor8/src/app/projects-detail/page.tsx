8481b147dc698750be2721d2ab970037317f1e24
2ujin <suddenlydisappear200@gmail.com>
2023-11-27 16:30:08 +0000

    feat: completed detail page

--- client/collabor8/src/app/projects-detail/page.tsx
+++ client/collabor8/src/app/projects-detail/page.tsx
@@ -8,8 +8,14 @@ import IconOwner from "../../../public/icon-owner.svg";
 import IconTeamMember from "../../../public/icon-teammember.svg";
 import Image from "next/image";
 import ProfileDetailCard from "@/components/project-detail-card/project-detail-card";
+import ProjectDescCard from "@/components/project-desc-card/project-desc-card";
 
 export default function MyProjects() {
+  const tempData = {
+    desc: "Instructors, students, and admins can create, edit and follow courses, assign roles, request help. Instructors can keep track of student performance, feedback on assessments.",
+    info: "We well sell our project to Codeworks and become billionaires.",
+  };
+
   return (
     <section className="projects-page">
       <div className="projects">
@@ -19,6 +25,7 @@ export default function MyProjects() {
           </div>
           <div className="projects-page__projects">
             <ProjectCard btnLabel="Apply" project={projects[0]} />
+            <ProjectDescCard desc={tempData.desc} info={tempData.info} />
           </div>
         </div>
       </div>
