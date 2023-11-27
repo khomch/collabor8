4ae21fc5e4ca64471f64ee1b7e02a2f2390eda59
Kamil <kzmuda888@gmail.com>
2023-11-27 17:31:15 +0000

    feat: finished login and register pages logic, added display error messages sent from server

--- client/collabor8/src/app/login/page.tsx
+++ client/collabor8/src/app/login/page.tsx
@@ -3,8 +3,10 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import Input from '../../components/input/input';
 import Button from '../../components/button/button';
+import { login } from '../../apiService/userService';
 import './page.css';
 
 export default function Login() {
@@ -12,10 +14,30 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [error, setError] = useState({ isError: false, errorMessage: "" });
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Submitted!');
-    // TODO: Add login functionality
+    const user = {
+      emailAddress: email,
+      password: password
+    }
+    const response = await login(user);
+    if (response.message) {
+      setError({ isError: true, errorMessage: response.message });
+      setEmail('');
+      setPassword('');
+      return;
+    } else {
+    localStorage.setItem('token', response);
+    setError({ isError: false, errorMessage: "" });
+    setEmail('');
+    setPassword('');
+    router.push('/');
+    // This might need some extra steps as the app grows
+    }
   };
 
   return (
@@ -33,6 +55,7 @@ export default function Login() {
         <div className='login-container'>
           <form onSubmit={handleSubmit} className='login-form'>
             <Input
+              required={true}
               type='email'
               name='email'
               label='Email'
@@ -44,6 +67,7 @@ export default function Login() {
               }}
             />
             <Input
+              required={true}
               type='password'
               name='password'
               label='Password'
@@ -54,6 +78,13 @@ export default function Login() {
                 setPassword(e.target.value);
               }}
             />
+            {error.isError && (
+              <div className="login-form__error">
+                <span className="bodytext3 bodytext3_semibold error">
+                  {error.errorMessage}
+                </span>
+              </div>
+            )}
             <div className="login-form__button">
               <Button
                 variant='primary'
---
+++ client/collabor8/src/types/types.ts
@@ -0,0 +1,12 @@
+export type LoginDetails = {
+  emailAddress: string,
+  password: string
+}
+
+export type RegisterDetails = {
+  emailAddress: string,
+  userName: string,
+  password: string,
+  firstName: string,
+  lastName: string
+}
