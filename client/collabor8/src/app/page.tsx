8c53d4b63374eb8a99385d3cec45931ea11d5e7d
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-11-27 08:46:53 +0000

    styled: add member component

--- client/collabor8/src/app/page.tsx
+++ client/collabor8/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Member from "@/components/member/member";
 import Link from "next/link";
 
 export default function LandingPage() {
@@ -6,6 +7,7 @@ export default function LandingPage() {
     <>
       <h1 className="bodytext1">Landing Page / Login</h1>
       <Link href="/register">Register</Link>
+      <Member icon="😵‍💫" />
     </>
   );
 }
---
+++ client/collabor8/src/components/member/member.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import person from "../../../public/person.svg";
+import "./member.css";
+import Image from "next/image";
+
+export type MemberProps = {
+  icon: "😵‍💫" | "😎" | "🥹" | "😅" | "🤓" | "😉";
+};
+
+function Member({ icon }: MemberProps) {
+  return (
+    <div className="member">
+      <Image className="member__image" src={person} />
+      <div className="member__emoji">{icon}</div>
+    </div>
+  );
+}
+
+export default Member;
