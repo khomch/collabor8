8144a3787a34722ed5878adcd748ff0689bd70f4
Kamil <kzmuda888@gmail.com>
2023-12-05 09:03:26 +0000

    fix: aligned logout button with other items, testing pages

--- client/collabor8/src/components/navbar/navbar.tsx
+++ client/collabor8/src/components/navbar/navbar.tsx
@@ -1,44 +1,44 @@
-'use client';
+"use client";
 
-import { useDispatch, useSelector } from '@/redux-store/customHooks';
-import { fetchUserDetails } from '@/redux-store/slices/userSlice';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
-import LogoSmallYellow from '../../../public/logo-yellow.png';
-import './navbar.css';
-import Button from '../button/button';
-import { useRouter } from 'next/navigation';
-import { resetUserState } from '@/redux-store/slices/userSlice';
+import { useDispatch, useSelector } from "@/redux-store/customHooks";
+import { fetchUserDetails } from "@/redux-store/slices/userSlice";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import LogoSmallYellow from "../../../public/logo-yellow.png";
+import "./navbar.css";
+import Button from "../button/button";
+import { useRouter } from "next/navigation";
+import { resetUserState } from "@/redux-store/slices/userSlice";
 
 const menuItems = [
   {
-    label: 'Home',
-    path: '/',
+    label: "Home",
+    path: "/",
   },
   {
-    label: 'Dashboard',
-    path: '/dashboard',
+    label: "Dashboard",
+    path: "/dashboard",
   },
   {
-    label: 'My Projects',
-    path: '/projects',
+    label: "My Projects",
+    path: "/projects",
   },
   {
-    label: 'Profile',
-    path: '/profile',
+    label: "Profile",
+    path: "/profile",
   },
 ];
 
 const unauthMenuItems = [
   {
-    label: 'Login',
-    path: '/login',
+    label: "Login",
+    path: "/login",
   },
   {
-    label: 'Register',
-    path: '/register',
+    label: "Register",
+    path: "/register",
   },
 ];
 
@@ -52,15 +52,15 @@ function Navbar() {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('accessToken');
+    localStorage.removeItem("accessToken");
     dispatch(resetUserState());
-    router.push('/login');
+    router.push("/login");
   };
   const pathname = usePathname();
   return (
     <nav className="navbar">
       <div className="navbar__container">
-        <Link href={'/'}>
+        <Link href={"/"}>
           <Image
             src={LogoSmallYellow}
             alt="Collabor8 Logo"
@@ -76,7 +76,7 @@ function Navbar() {
                 {menuItems.map((item, index) => (
                   <li
                     className={`navbar__menu-item ${
-                      pathname === item.path && 'navbar__menu-item_active'
+                      pathname === item.path && "navbar__menu-item_active"
                     }`}
                     key={item.path}
                   >
@@ -88,13 +88,15 @@ function Navbar() {
                     </Link>
                   </li>
                 ))}
-                <li className="navbar__menu-link">
-                  <Button
-                    variant="primary"
-                    label="Logout"
-                    onClick={() => handleLogout()}
-                    type="button"
-                  />
+                <li className="navbar__menu-item">
+                  <div className="navbar__menu-link">
+                    <Button
+                      variant="primary"
+                      label="Logout"
+                      onClick={() => handleLogout()}
+                      type="button"
+                    />
+                  </div>
                 </li>
               </>
             ) : (
@@ -102,7 +104,7 @@ function Navbar() {
                 {unauthMenuItems.map((item, index) => (
                   <li
                     className={`navbar__menu-item ${
-                      pathname === item.path && 'navbar__menu-item_active'
+                      pathname === item.path && "navbar__menu-item_active"
                     }`}
                     key={item.path}
                   >
